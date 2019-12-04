acf20a86e1db0852793916e8c59c34ffef5d4620
Waïss Azizian <waissfowl@gmail.com>
2019-12-04 10:43:24 +0100

    Add Siamese wrapper

---
+++ models/siamese.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class Siamese(nn.Module):
+    def __init__(module: nn.Module):
+        super(Siamese, self).__init__()
+        self.gnn = module
+
+    def forward(self, g1, g2):
+        emb1, emb2 = self.gnn(g1), self.gnn(g2)
+        #here emb.shape=(bs, n_vertices, n_features)
+        out = torch.bmm(emb1, emb2.permute(0, 2, 1))
+        return out
