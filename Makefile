d7bce7bdd5fecb2fb97db432f73b368bc07a1a8f
unverrable <r.laurent@outlook.fr>
2019-12-11 12:06:16 +0100

    Fix generation of log files

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ quick_test:
 	python3 train/train.py --num_examples_test 100 --mode test
 
 quick_experiment:
-	python3 train/train.py --num_examples_train 100 --num_examples_test 100 --print_freq 100 --epoch 2 --mode experiment
+	python3 train/train.py --expressive_suffix True --num_examples_train 100 --num_examples_test 100 --print_freq 100 --epoch 2 --mode experiment
 
 clean:
 	rm -rf dataset/*
--- train/experiment.py
+++ train/experiment.py
@@ -5,9 +5,10 @@ import json
 def save_experiment(args, acc):
     git_commit = os.popen('git describe --always').read()
     d = vars(args)
-    d['commit'] = git_commit
+    d['commit'] = git_commit.replace("\n", "")
     now = datetime.datetime.now()
     d['end'] = now.strftime("%d/%m/%Y %H:%M:%S")
     d['accuracy'] = acc
     with open(args.path_exp, 'a') as f:
         json.dump(d, f)
+        f.write("\n")
