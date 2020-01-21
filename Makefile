a38bd2037a2e50b29623d97c01040965c886f310
Waïss Azizian <waissfowl@gmail.com>
2020-01-21 10:39:55 +0000

    Fix dl_test bug (and add Make command)

--- Makefile
+++ Makefile
@@ -20,7 +20,7 @@ overfit:
 	python3 train/train.py --expressive_suffix False --num_examples_train 5 --num_examples_test 995 --print_freq 1000 --epoch 50 --lr 0.01 --gamma 0.75 --overfit --classification --num_layers 2 --num_features 2 --num_blocks 1
 
 experiment:
-	python3 train/train.py --num_examples_train 20000 --num_examples_test 1000 --mode experiment --print_freq 1000 --epoch 100 --step_epoch 5 --num_blocks 2 --generative_model Regular --noise 0.05 --lr 1e-3 --gamma 0.9 --num_features 64 --num_layers 3
+	python3 train/train.py --num_examples_train 750 --num_examples_test 1 --num_examples_val 249 --n_vertices 136 --print_freq 100 --epoch 50 --mode experiment --lr 0.001 --num_layers 3 --num_blocks 2 --num_features 32 --real_world_dataset --gamma 0.9
 
 clean:
 	rm -rf dataset/*
--- train/data_generator.py
+++ train/data_generator.py
@@ -252,7 +252,8 @@ def selfsupervised_dataloader(args, data_generator):
         dl = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
         return dl, dl, dl
     else:
-        test_dataset = torch.utils.data.Subset(dataset, torch.arange(args.num_examples_train + args.num_examples_val, len(dataset)))
+        assert args.num_examples_train + args.num_examples_val + args.num_examples_test == len(dataset)
+        test_dataset = dataset[args.num_examples_train + args.num_examples_val:]
         train_dataset, val_dataset = torch.utils.data.random_split(dataset[:(args.num_examples_train + args.num_examples_val)], [args.num_examples_train, args.num_examples_val])
         train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
         test_dl  = torch.utils.data.DataLoader( test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
